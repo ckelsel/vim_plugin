feeed831e2ab1e3964685a9670fb18db339613d7
xie.kunming <xie.kunming@astute-tec.com>
2018-09-22 11:58:54 +0800

    download ctags

--- install_vim_plug.ps1
+++ install_vim_plug.ps1
@@ -6,3 +6,11 @@ $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     "~\vimfiles\autoload\plug.vim"
   )
 )
+
+$uri = 'https://nchc.dl.sourceforge.net/project/ctags/ctags/5.8/ctags58.zip'
+(New-Object Net.WebClient).DownloadFile(
+  $uri,
+  $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+    "~\vimfiles\ctags.zip"
+  )
+)
